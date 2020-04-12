2381e9ce7cd236a362c2f0b37df160d66aea1fd6
dikywahyudi25 <18523202@students.uii.ac.id>
2020-04-12 16:06:09 +0700

    First commit

--- Assets/ballscript.cs
+++ Assets/ballscript.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class ballscript : MonoBehaviour
 {
 	public int speed=20;
+
+	public Rigidbody2D sesuatu;
+
+	public Animation anim;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
-        GetComponent<Rigidbody2D>().velocity = new Vector2(1,-1) * speed;
+        
+        sesuatu.velocity = new Vector2(1,-1) * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (sesuatu.velocity.x > 0){ // bergerak ke kanan 
+        	sesuatu.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        }else{
+        	sesuatu.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+
         
     }
 }
